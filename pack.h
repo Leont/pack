43232d8985a6a8b59ee35599302f6f328d45b616
Leon Timmermans <fawaka@gmail.com>
2016-05-29 01:32:17 +0200

    Make unpacking single-phased

--- pack.h
+++ pack.h
@@ -34,7 +34,7 @@ namespace pack {
 				return head_type::pack(std::move(data)) + follow_up<tail_types...>::pack(arguments...);
 			}
 			static std::tuple<typename head_type::data_type, typename tail_types::data_type...> unpack(std::string::const_iterator current, const std::string::const_iterator& end) {
-				auto first = std::make_tuple(head_type::unpack(current, end).decode());
+				auto first = std::make_tuple(head_type::unpack(current, end));
 				return tuple_cat(std::move(first), follow_up<tail_types...>::unpack(current, end));
 			}
 		};
@@ -43,7 +43,7 @@ namespace pack {
 				return head_type::pack(data);
 			}
 			static std::tuple<typename head_type::data_type> unpack(std::string::const_iterator current, const std::string::const_iterator& end) {
-				return std::make_tuple(head_type::unpack(current, end).decode());
+				return std::make_tuple(head_type::unpack(current, end));
 			}
 		};
 
@@ -61,39 +61,28 @@ namespace pack {
 				*target++ = *current++;
 		}
 
-		template<enum endian order> void byte_copy(char* target, const std::string::const_iterator& begin, const std::string::const_iterator& end) {
+		template<enum endian order> void byte_copy(char* target, const std::string::const_iterator& begin, const std::string::const_iterator& end) noexcept {
 			return byte_copy<order>(target, &*begin, &*end);
 		}
 	}
 
-	struct piece {
-		const std::string::const_iterator begin;
-		const std::string::const_iterator end;
-		piece(const std::string::const_iterator& _begin, const std::string::const_iterator& _end) : begin(_begin), end(_end) { }
-	};
-
 	template<typename raw_type, endian order = endian::native> struct integral {
 		using data_type = raw_type;
-		struct decoder : public piece {
-			using piece::piece;
-			raw_type decode() const noexcept {
-				raw_type temp(0);
-				byte_copy<order>(reinterpret_cast<char*>(&temp), begin, end);
-				return temp;
-			}
-		};
 		static std::string pack(raw_type value) noexcept {
 			char buffer[sizeof(raw_type)];
 			byte_copy<order>(buffer, reinterpret_cast<const char*>(&value), reinterpret_cast<const char*>(&value + 1));
 			return std::string(buffer, buffer + sizeof buffer);
 		}
-		static decoder unpack(std::string::const_iterator& current, const std::string::const_iterator& end) {
+		static data_type unpack(std::string::const_iterator& current, const std::string::const_iterator& end) {
 			static const std::string null(sizeof(raw_type), '\0');
 
 			if (current + sizeof(raw_type) <= end) {
 				auto begin = current;
 				current += sizeof(raw_type);
-				return decoder(begin, current);
+
+				raw_type temp(0);
+				byte_copy<order>(reinterpret_cast<char*>(&temp), begin, current);
+				return temp;
 			}
 			else
 				throw exception::out_of_bounds("integer");
@@ -105,19 +94,6 @@ namespace pack {
 		using data_type = raw_type;
 		static const size_t block_size = 1 << 7;
 		static const size_t mask = block_size - 1;
-
-		struct decoder : public piece {
-			using piece::piece;
-			raw_type decode() const noexcept {
-				raw_type factor = 1, ret = 0;
-				auto current = begin;
-				while (current < end) {
-					ret += (static_cast<unsigned char>(*current++) & mask) * factor;
-					factor *= block_size;
-				}
-				return ret;
-			}
-		};
 
 		static std::string pack(raw_type value) noexcept {
 			std::string ret;
@@ -132,13 +108,19 @@ namespace pack {
 				*(ret.end() - 1) &= mask;
 			return ret;
 		}
-		static decoder unpack(std::string::const_iterator& current, const std::string::const_iterator& end) {
+		static data_type unpack(std::string::const_iterator& current, const std::string::const_iterator& end) {
 			std::string::const_iterator begin = current;
-			while (static_cast<unsigned char>(*current++) & block_size) {
+			raw_type factor = 1, ret = 0;
+			while (1) {
 				if (current == end)
 					throw exception::out_of_bounds("compressed integer");
+				unsigned char value = static_cast<unsigned char>(*current++);
+				ret += (value & mask) * factor;
+				factor *= block_size;
+				if (!(value & block_size))
+					break;
 			}
-			return decoder(begin, current);
+			return ret;
 		}
 	};
 
@@ -179,20 +161,14 @@ namespace pack {
 
 	template<int length, typename pad = padding::none> struct fixed_string {
 		using data_type = std::string;
-		struct decoder : public piece {
-			using piece::piece;
-			std::string decode() const noexcept {
-				return pad::strip_padding(std::string(begin, end));
-			}
-		};
 		static std::string pack(std::string value) {
 			return pad::add_padding(value, length);
 		}
-		static decoder unpack(std::string::const_iterator& current, const std::string::const_iterator& end) {
+		static std::string unpack(std::string::const_iterator& current, const std::string::const_iterator& end) {
 			if (current + length <= end) {
 				auto begin = current;
 				current += length;
-				return decoder(begin, current);
+				return pad::strip_padding(std::string(begin, current));
 			}
 			else
 				throw exception::out_of_bounds("fixed_string");
@@ -201,21 +177,15 @@ namespace pack {
 
 	template<typename length_encoder = integral<unsigned, endian::little>> struct varchar {
 		using data_type = std::string;
-		struct decoder : public piece {
-			using piece::piece;
-			std::string decode() const noexcept {
-				return std::string(begin, end);
-			}
-		};
 		static std::string pack(std::string value) {
 			return length_encoder::pack(value.size()) + value;
 		}
-		static decoder unpack(std::string::const_iterator& current, const std::string::const_iterator& end) {
-			size_t length = length_encoder::unpack(current, end).decode();
+		static std::string unpack(std::string::const_iterator& current, const std::string::const_iterator& end) {
+			size_t length = length_encoder::unpack(current, end);
 			if (unsigned(end - current) >= length) {
 				auto begin = current;
 				current += length;
-				return decoder(begin, current + length);
+				return std::string(begin, current);
 			}
 			else
 				throw exception::out_of_bounds("varchar");
@@ -223,31 +193,20 @@ namespace pack {
 	};
 
 	template<typename element_encoder, typename length_encoder> struct sequence {
-		class decoder {
-			using element_type = decltype(std::declval<element_encoder::decoder>().decode());
-			using decoder_type = typename element_encoder::decoder;
-			std::vector<decoder_type> elems;
-			public:
-			decoder(std::vector<decoder_type> _elems) : elems(_elems) { }
-			std::vector<element_type> decode() const {
-				std::vector<element_type> ret;
-				for (const auto& piece : elems)
-					ret.push_back(ret.decode());
-				return ret;
-			}
-		};
+		using element_type = typename element_encoder::data_type;
+		using data_type = std::vector<element_type>;
 		template<typename argument_type> static std::string pack(const std::vector<argument_type>& elements) {
 			std::string ret = length_encoder::pack(elements.size());
 			for (const auto& elem: elements)
 				ret += element_encoder::pack(elem);
 			return ret;
 		}
-		static decoder unpack(std::string::const_iterator& current, const std::string::const_iterator& end) {
-			size_t length = length_encoder::unpack(current, end).decode();
-			std::vector<piece> pieces;
+		static std::vector<element_type> unpack(std::string::const_iterator& current, const std::string::const_iterator& end) {
+			size_t length = length_encoder::unpack(current, end);
+			std::vector<element_type> pieces;
 			while (length--)
 				pieces.push_back(element_encoder::unpack(current, end));
-			return decoder(std::move(pieces));
+			return pieces;
 		}
 	};
 
