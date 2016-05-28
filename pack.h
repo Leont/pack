6f6dccd41a9c17f9423ed9c0923f924f84beba82
Leon Timmermans <fawaka@gmail.com>
2016-05-28 15:04:14 +0200

    Move decoding into follow_up

--- pack.h
+++ pack.h
@@ -21,16 +21,17 @@ namespace pack {
 			template<typename head_argument, typename... tail_arguments> static std::string pack(const head_argument& data, const tail_arguments&... arguments) {
 				return head_type::pack(std::move(data)) + follow_up<tail_types...>::pack(arguments...);
 			}
-			static std::tuple<typename head_type::decoder, typename tail_types::decoder...> unpack(std::string::const_iterator current, const std::string::const_iterator& end) {
-				return tuple_cat(std::make_tuple(head_type::unpack(current, end)), follow_up<tail_types...>::unpack(current, end));
+			static std::tuple<typename head_type::data_type, typename tail_types::data_type...> unpack(std::string::const_iterator current, const std::string::const_iterator& end) {
+				auto first = std::make_tuple(head_type::unpack(current, end).decode());
+				return tuple_cat(std::move(first), follow_up<tail_types...>::unpack(current, end));
 			}
 		};
 		template<typename head_type> struct follow_up<head_type> {
 			template<typename head_argument> static std::string pack(const head_argument& data) {
 				return head_type::pack(data);
 			}
-			static std::tuple<typename head_type::decoder> unpack(std::string::const_iterator current, const std::string::const_iterator& end) {
-				return std::make_tuple(head_type::unpack(current, end));
+			static std::tuple<typename head_type::data_type> unpack(std::string::const_iterator current, const std::string::const_iterator& end) {
+				return std::make_tuple(head_type::unpack(current, end).decode());
 			}
 		};
 
@@ -60,6 +61,7 @@ namespace pack {
 	};
 
 	template<typename raw_type, endian order = endian::native> struct integral {
+		using data_type = raw_type;
 		struct decoder : public piece {
 			using piece::piece;
 			raw_type decode() const noexcept {
@@ -90,6 +92,7 @@ namespace pack {
 
 	template<typename raw_type = unsigned long long, endian order = endian::little> struct compressed;
 	template<typename raw_type> struct compressed<raw_type, endian::little> {
+		using data_type = raw_type;
 		static const size_t block_size = 1 << 7;
 		static const size_t mask = block_size - 1;
 
@@ -132,6 +135,7 @@ namespace pack {
 	enum class padding { null };
 
 	struct stringer {
+		using data_type = std::string;
 		struct decoder : public piece {
 			using piece::piece;
 			std::string decode() const {
@@ -204,23 +208,14 @@ namespace pack {
 
 	template<typename... elements> class format {
 		typedef follow_up<elements...> packer;
-		constexpr size_t size = sizeof...(elements);
 
-		template<typename decoder> static auto decode(const decoder& decoding) -> decltype(decoding.decode()) {
-			return decoding.decode();
-		}
-		template<typename tuple, size_t... I> static auto decode_all(const tuple& decoders, std::index_sequence<I...> ) -> std::tuple<decltype(decode(std::get<I>(decoders)))...> {
-			return std::make_tuple(decode(std::get<I>(decoders))...);
-		}
 		public:
 		template<typename... argument_types> static std::string pack(const argument_types&... arguments) {
 			return packer::pack(arguments...);
 		}
-		static auto unpack(const std::string& packed) {
+		static std::tuple<typename elements::data_type...> unpack(const std::string& packed) {
 			std::string::const_iterator begin = packed.begin();
-			auto plan = packer::unpack(begin, packed.end());
-			using Indices = std::make_index_sequence<std::tuple_size<std::decay_t<decltype(plan)>>::value>;
-			return decode_all(plan, Indices{});
+			return packer::unpack(begin, packed.end());
 		}
 	};
 }
